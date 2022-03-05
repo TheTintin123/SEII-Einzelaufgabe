417479366224e873d1bed89b434d73513c1e84a6
thetintin123 <kobenischke@edu.aau.at>
2022-03-05 12:12:24 +0100

    Implementation

---
+++ Aufgabe0/app/src/main/java/com/example/aufgabe0/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.aufgabe0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.sql.Array;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void button1(View view){
+        Toast.makeText(this,"Button Clicked!", Toast.LENGTH_SHORT).show();
+        EditText txt = findViewById(R.id.inputNumber);
+    }
+
+
+
+}
