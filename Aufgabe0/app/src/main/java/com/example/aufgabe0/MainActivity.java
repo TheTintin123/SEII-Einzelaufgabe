7474cb8d22dede0173f8cb65a25012394cfb5ad7
TheTintin123 <kobenischke@edu.aau.at>
2022-03-13 22:13:36 +0100

    Adding a progress bar

--- Aufgabe0/app/src/main/java/com/example/aufgabe0/MainActivity.java
+++ Aufgabe0/app/src/main/java/com/example/aufgabe0/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,14 +16,23 @@ import java.util.Arrays;
 
 import java.net.*;
 import java.io.*;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    int counter = 0;
+    String calcOutputTxt, serverOutputTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView calcOutputField = findViewById(R.id.outputField);
+        TextView serverOutputField = findViewById(R.id.outputServer);
+        calcOutputTxt = (String) calcOutputField.getText();
+        serverOutputTxt = (String) serverOutputField.getText();
     }
 
     public void onConnectionButtonClick(View view){
@@ -31,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onCalcButtonClick(View view){
+        progress();
         Toast.makeText(this,"CalcButton Clicked!", Toast.LENGTH_SHORT).show();
         EditText txt = findViewById(R.id.inputNumber);
         StringBuilder output = new StringBuilder();
@@ -48,8 +59,30 @@ public class MainActivity extends AppCompatActivity {
             if(i >= 0)
                 output.append(i);
         }
-        TextView outputField = findViewById(R.id.outputField);
-        outputField.setText(output);
+        calcOutputTxt = output.toString();
+    }
+
+    private void progress(){
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
+        TextView calcOutputField = findViewById(R.id.outputField);
+        TextView serverOutputField = findViewById(R.id.outputServer);
+        final Timer timer = new Timer();
+        TimerTask tt = new TimerTask() {
+            @Override
+            public void run() {
+                counter++;
+                progressBar.setProgress(counter*4);
+                if(counter == 25){
+                    calcOutputField.setText(calcOutputTxt);
+                    serverOutputField.setText(serverOutputTxt);
+                    timer.cancel();
+                    counter = 0;
+                }
+
+            }
+        };
+        timer.schedule(tt,0,100);
     }
 
     public boolean isPrime(int digit) {
@@ -67,6 +100,11 @@ public class MainActivity extends AppCompatActivity {
     private class ClientThreader extends AsyncTask<Void,Void,Void>{
 
         private String response = "";
+        @Override
+        protected void onPreExecute() {
+            progress();
+        }
+
         @Override
         protected Void doInBackground(Void... voids){
 
@@ -124,8 +162,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void aVoid){
             super.onPostExecute(aVoid);
-            TextView responseField = findViewById(R.id.outputServer);
-            responseField.setText(response);
+            serverOutputTxt = response;
         }
 
     }
